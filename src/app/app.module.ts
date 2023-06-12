64564faeb210fcdd6f93aec2578a45c2a49d09bb
dkholodov <dkhol@inbox.ru>
2023-06-12 19:53:01 +0300

    #AUTOSPOT-TEST: Реализована базовая логика приложения согласно ТЗ без адаптива

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
-import { NgModule } from '@angular/core';
+import { NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewsListComponent } from './components/news-list/news-list.component';
+import { NewsPageComponent } from './components/news-page/news-page.component';
+import { AddNewsModalComponent } from './components/shared/add-news-modal/add-news-modal.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {NgOptimizedImage} from "@angular/common";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsListComponent,
+    NewsPageComponent,
+    AddNewsModalComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    NgOptimizedImage,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
